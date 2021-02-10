c74daf8cd9913ef757a17d903a7ef95fb7cd52fc
Mark Camball <mark.camball@gmail.com>
2021-02-09 22:20:40 -0500

    more files

--- pages/failing.jsx
+++ pages/failing.jsx
@@ -1,11 +1,24 @@
-import withUser from "../components/auth/with-user"
-
+import BigFile1 from "../components/BigFile1"
+// import BigFile2 from "../components/BigFile2"
+// import BigFile3 from "../components/BigFile3"
+// import BigFile4 from "../components/BigFile4"
+// import BigFile5 from "../components/BigFile5"
+// import BigFile6 from "../components/BigFile6"
+// import BigFile7 from "../components/BigFile7"
+// import BigFile8 from "../components/BigFile8"
 function Home() {
   return (
     <>
-      Hello worldx
+      <BigFile1/>
+      {/*<BigFile2/>*/}
+      {/*<BigFile3/>*/}
+      {/*<BigFile4/>*/}
+      {/*<BigFile5/>*/}
+      {/*<BigFile6/>*/}
+      {/*<BigFile7/>*/}
+      {/*<BigFile8/>*/}
     </>
   )
 }
 
-export default withUser(Home)
+export default Home
